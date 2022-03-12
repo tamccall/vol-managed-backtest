b58d73d4e7a6df8a74049b1894c74836764a6403
tamccall <andymc903@gmail.com>
2022-03-12 11:19:20 -0800

    black formatting

--- __init__.py
+++ __init__.py
@@ -21,9 +21,10 @@ class TSDWeights(bt.algos.Algo):
         self.weights = weights
 
 
-treasuries = pd.read_csv('daily-treasury-rates.csv', index_col=0)
+treasuries = pd.read_csv("daily-treasury-rates.csv", index_col=0)
 treasuries = treasuries.set_index(pd.to_datetime(treasuries.index)).sort_index() / 100
 two_year = treasuries["2 Yr"]
+
 
 def vol_managed_potfolio_vix(data, c, max_leverage=2):
     spy_ret = pd.Series(np.diff(np.log(data.spy)), index=data.spy.index[1:])
@@ -45,24 +46,33 @@ def vol_managed_potfolio_vix(data, c, max_leverage=2):
         ],
     )
 
+
 def vol_managed_potfolio(data, c, max_leverage=2):
     spy_ret = pd.Series(np.diff(np.log(data.spy)), index=data.spy.index[1:])
     excess_ret = np.power(1 + spy_ret, 365.25) - two_year
-    realized_std = spy_ret.rolling(21).std() * math.sqrt(253) # get the rolling standard deviations
-    realized_var = np.power(realized_std, 2) # square it to come up with the variance
-    f = c / realized_var * excess_ret.shift(1) # formula from https://onlinelibrary.wiley.com/doi/abs/10.1111/jofi.12513
+    realized_std = spy_ret.rolling(21).std() * math.sqrt(
+        253
+    )  # get the rolling standard deviations
+    realized_var = np.power(realized_std, 2)  # square it to come up with the variance
+    f = (
+        c / realized_var * excess_ret.shift(1)
+    )  # formula from https://onlinelibrary.wiley.com/doi/abs/10.1111/jofi.12513
     cond_ret = f + two_year
 
     # in your paper you mention the optimal weight being proportional to the risk return trade off
     # this attempts to capture that.
-    risk_ret_trade = np.minimum(cond_ret / realized_var, max_leverage) # this strategy seems to go bankrupt if we let it get too crazy with the leverage
+    risk_ret_trade = np.minimum(
+        cond_ret / realized_var, max_leverage
+    )  # this strategy seems to go bankrupt if we let it get too crazy with the leverage
 
     # we put the rest of the allocation into some risk-free investment
     # but we don't short the bond etf
     risk_free_weight = np.maximum(1 - risk_ret_trade, 0)
 
-
-    weights = {"spy": risk_ret_trade, "vgsh": risk_free_weight} # a map of weights that we allocate to each ticker overtime
+    weights = {
+        "spy": risk_ret_trade,
+        "vgsh": risk_free_weight,
+    }  # a map of weights that we allocate to each ticker overtime
     return bt.Strategy(
         "vol_managed",
         [
@@ -73,6 +83,7 @@ def vol_managed_potfolio(data, c, max_leverage=2):
             bt.algos.Rebalance(),
         ],
     )
+
 
 def eighty_twenty():
 
@@ -86,6 +97,7 @@ def eighty_twenty():
         ],
     )
 
+
 def fictional_vix():
 
     return bt.Strategy(
@@ -97,6 +109,7 @@ def fictional_vix():
             bt.algos.Rebalance(),
         ],
     )
+
 
 def buy_and_hold():
 
@@ -110,15 +123,17 @@ def buy_and_hold():
         ],
     )
 
+
 data = bt.get("^vix, spy, vgsh", start="2017-01-01", end="2022-03-11")
+
 
 def run_backtests():
     tests = [
-        bt.Backtest(vol_managed_potfolio_vix(data, 0.00426746700832415, max_leverage=1.5), data),
-        bt.Backtest(vol_managed_potfolio(data, 0.00426746700832415, max_leverage=1.5), data),
+        bt.Backtest(vol_managed_potfolio_vix(data, 0.00426746700832415), data),
+        bt.Backtest(vol_managed_potfolio(data, 0.00426746700832415), data),
         bt.Backtest(eighty_twenty(), data),
         bt.Backtest(buy_and_hold(), data),
-        bt.Backtest(fictional_vix(), data)
+        bt.Backtest(fictional_vix(), data),
     ]
 
     res = bt.run(*tests)
@@ -126,5 +141,6 @@ def run_backtests():
 
     plot = res.plot()
     plot.figure.show()
+
 
 run_backtests()
